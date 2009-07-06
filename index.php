f2151d5ef1609fd66483a417ead2e732b6f56c76
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-07-06 15:32:38 -0700

    - Added jquery and curvycorners - Started structuring page layout - Added background gradient

--- index.php
+++ index.php
@@ -1,1 +1,55 @@
-phx2600.org
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>PHX2600 - The Phoenix, Arizona Network of Hackers</title>
+	<link rel="stylesheet" type="text/css" href="/css/reset.css" />
+	<link rel="stylesheet" type="text/css" href="/css/base.css" />
+	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+	<link rel="shortcut icon" href="/images/icons/folder_explore.png" />
+
+	<meta name="keywords" content="Phoenix, Arizona, Network, Hackers, Hacking, Social Engineering, Urban Exploration, Lock Picking" />
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
+	<script type="text/javascript" src="/js/jquery.js"></script>
+	<script type="text/javascript" src="/js/jquery.curvycorners.js"></script>
+
+	<script type="text/javascript">
+	$(document).ready(function(){
+		$('.rounded').corner();
+		$('.rounded-top').corner({
+			tl: {radius: 8},
+			tr: {radius: 8},
+			bl: false,
+			br: false
+		});
+		$('.rounded-bottom').corner({
+			tl: false,
+			tr: false,
+			bl: {radius: 8},
+			br: {radius: 8}
+		});
+	});
+	</script>
+</head>
+
+<body>
+
+<div id="wrap">
+
+	<div id="header" class="rounded-top">Header</div>
+
+	<div id="navigation">Navigation</div>
+
+	<div id="body">
+		<div id="content">Content</div>
+		<div id="right-nav">Right Nav</div>
+	</div>
+
+	<div id="footer" class="rounded-bottom">Footer</div>
+
+</div>
+
+</body>
+</html>
