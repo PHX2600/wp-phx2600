46416abe13e7f29722db6a3ccae02a349e56a154
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-07-15 22:40:21 -0700

    Added header

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+	<title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
+	<!--[if lte IE 8]>
+		<style type="text/css">#search button { padding: 8px; }</style>
+	<![endif]-->
+    <!--[if lte IE 7]>
+    	<style type="text/css">#search button { padding: 6px; }</style>
+    <![endif]-->
+
+	<link rel="shortcut icon" href="/images/icons/folder_explore.png" />
+	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+	<meta name="keywords" content="Phoenix, Arizona, Network, Hackers, Hacking, Social Engineering, Urban Exploration, Lock Picking" />
+	<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+
+	<?php wp_head(); ?>
+</head>
+
+<body>
+
+<div id="wrap">
