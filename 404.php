653a68f59bf1200e7f53a83becdf2e1620a78e30
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-07-25 17:14:20 -0700

    Really added 404.php and page.php

---
+++ 404.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+			<div class="hentry">
+				<h2 class="post-title">Page Not Found</h2><br />
+				Sorry, but you are looking for something that isn't here.
+			</div>
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
+
+<?php get_footer(); ?>
---
+++ page.php
@@ -0,0 +1,33 @@
+<?php get_header(); ?>
+
+			<?php if (have_posts()): ?>
+				<?php while (have_posts()): the_post(); ?>
+
+					<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<h2 class="post-title"><a href="<?php the_permalink(); ?>" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<br />
+						<div class="post-content clearfix">
+							<?php the_content(); ?>
+						</div>
+						<span class="edit"><?php edit_post_link('Edit', '', ''); ?></span>
+					</div>
+
+				<?php endwhile; ?>
+
+				<div id="pagination">
+					<?php if(function_exists('wp_pagenavi')) { wp_pagenavi(); } ?>
+				</div>
+
+			<?php else: ?>
+				<div class="hentry">
+					<h2 class="post-title">Error</h2>
+						<br />
+						Sorry, but you are looking for something that isn't here.
+					</div>
+			<?php endif; ?>
+
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
+
+<?php get_footer(); ?>
