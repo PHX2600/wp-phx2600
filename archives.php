7d43b0245c3947eaf9d4693fd06d824d4b57cf8e
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2009-07-29 00:08:20 -0700

    Archive style changes

--- archives.php
+++ archives.php
@@ -5,14 +5,15 @@ Template Name: Archives
 ?>
 <?php get_header(); ?>
 
-			<div id="entry">
-				<h2>Archives by Month:</h2>
+			<div class="hentry">
+				<h2 class="post-title">Archive</h2>
+				<h3>Archives by Month:</h3>
 				<ul>
 					<?php wp_get_archives('type=monthly'); ?>
 					<?php // wp_easyarchives(); ?>
 				</ul>
 
-				<h2>Archives by Subject:</h2>
+				<h3>Archives by Subject:</h3>
 				<ul>
 					<?php wp_list_categories(); ?>
 				</ul>
